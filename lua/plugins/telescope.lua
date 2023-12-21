b9aba01fdc8871e4dd65957f2bebc798d461b5ba
Alejandro Henestroza <alehenestroza@gmail.com>
2023-12-20 21:54:06 -0300

    Add lsp-config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,32 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require("telescope.builtin")
-        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', function()
-            builtin.buffers({
-                sort_mru = true,
-                ignore_current_buffer = true,
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', function()
+                builtin.buffers({
+                    sort_mru = true,
+                    ignore_current_buffer = true,
+                })
+            end)
+        end
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {}
+                    },
+                },
             })
-        end)
-    end
+            require("telescope").load_extension("ui-select")
+        end
+    },
 }
 
