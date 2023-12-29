fcd7d5e867955c5b718bb17c521c50c9a79fcdc0
Alejandro Henestroza <alehenestroza@gmail.com>
2023-12-28 22:23:10 -0300

    Add transparency, remap harpoon to not conflict with tmux. Remap telescope

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,15 +2,17 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    opts = {
-        integrations = {
-            neotree     = true,
-            telescope   = true,
-            treesitter  = true,
-        },
-    },
     config = function()
+        require("catppuccin").setup({
+            integrations = {
+                neotree = true,
+                telescope = true,
+                treesitter = true,
+            },
+            transparent_background = true,
+        })
+
         vim.o.termguicolors = true
-        vim.cmd.colorscheme "catppuccin-macchiato"
-    end
+        vim.cmd.colorscheme("catppuccin-macchiato")
+    end,
 }
