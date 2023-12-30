e33003431c9ecedd8209a30972c2d63c8292cfd2
Alejandro Henestroza <alehenestroza@gmail.com>
2023-12-30 17:54:55 -0300

    add alpha dashboard configuration

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,6 +1,37 @@
 return {
-	"goolord/alpha-nvim",
-	config = function()
-		require("alpha").setup(require("alpha.themes.dashboard").config)
-	end,
+    "goolord/alpha-nvim",
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+
+        dashboard.section.header.val = {
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                     ]],
+            [[       ████ ██████           █████      ██                     ]],
+            [[      ███████████             █████                             ]],
+            [[      █████████ ███████████████████ ███   ███████████   ]],
+            [[     █████████  ███    █████████████ █████ ██████████████   ]],
+            [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+            [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+            [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+        }
+
+        _Gopts = {
+            position = "center",
+            hl = "Type",
+            -- wrap = "overflow";
+        }
+
+        dashboard.opts.opts.noautocmd = true
+        alpha.setup(dashboard.opts)
+    end,
 }
