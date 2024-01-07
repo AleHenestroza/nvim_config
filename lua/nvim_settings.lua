80cbd46a4ee3dd96c651e0788137b9dd6d0c560e
Alejandro Henestroza <alehenestroza@gmail.com>
2024-01-07 18:26:57 -0300

    add current buffer to git

--- lua/nvim_settings.lua
+++ lua/nvim_settings.lua
@@ -4,8 +4,8 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "<leader>ga", "[[:!git add %:p<CR>]]", { noremap = true, silent = true })
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
-
-
