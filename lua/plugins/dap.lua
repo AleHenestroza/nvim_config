0c9d1bcdd5e45e6dabbfe8c706cf896d4d63dabf
Alejandro Henestroza <alehenestroza@gmail.com>
2024-01-11 19:55:32 -0300

    add nvim-dap

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,50 @@
+return {
+	{
+		"mfussenegger/nvim-dap",
+		config = function()
+			local dap = require("dap")
+
+			vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
+			vim.keymap.set("n", "<leader>dc", dap.continue, {})
+			vim.keymap.set("n", "<leader>dso", dap.step_over, {})
+			vim.keymap.set("n", "<leader>dsi", dap.step_into, {})
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"mfussenegger/nvim-dap",
+		},
+		config = function()
+			require("mason-nvim-dap").setup({
+				ensure_installed = { "delve" },
+			})
+		end,
+	},
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
+		config = function()
+			local dap, dapui = require('dap'), require("dapui")
+            dapui.setup()
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			vim.keymap.set("n", "<leader>dt", dapui.toggle, {})
+		end,
+	},
+}
