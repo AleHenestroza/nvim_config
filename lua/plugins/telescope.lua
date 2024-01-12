037465098b6eca718cd7bd820886b1ae504126e5
Alejandro Henestroza <alehenestroza@gmail.com>
2024-01-12 00:23:49 -0300

    add telescope frecency

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,12 +6,16 @@ return {
 		"smartpde/telescope-recent-files",
 	},
 	{
+		"nvim-telescope/telescope-frecency.nvim",
+	},
+	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope-ui-select.nvim",
 			"smartpde/telescope-recent-files",
+			"nvim-telescope/telescope-frecency.nvim",
 		},
 		config = function()
 			local telescope = require("telescope")
@@ -25,10 +29,15 @@ return {
 					recent_files = {
 						only_cwd = true,
 					},
+                    frecency = {
+                        default_workspace = "CWD",
+                        show_scores = true,
+                    },
 				},
 			})
 			telescope.load_extension("ui-select")
 			telescope.load_extension("recent_files")
+			telescope.load_extension("frecency")
 
 			-- Files / Buffers
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
@@ -39,6 +48,8 @@ return {
 				})
 			end)
 			vim.keymap.set("n", "<leader>fh", telescope.extensions.recent_files.pick)
+			vim.keymap.set("n", "<leader>fr", "<Cmd>Telescope frecency<CR>")
+
 			-- Git
 			vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
 			vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
